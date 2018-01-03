8f09a1d63054312ba28353d162f019e46854b8bf
Konrad Hernblad <konrad_hernblad@innovationstudio.vanguard>
2018-01-02 23:42:49 -0500

    reverted MyBot.js bot name back to 'MyBot'

--- MyBot.js
+++ MyBot.js
@@ -6,7 +6,7 @@ const {defaultStrategy, westernDuel} = require('./strategies/strategies');
 // start a game with a bot named 'JsBot'
 // and a strategy defaultStrategy defined in strategies.js
 // it is defined a separate file so you can unit test it in strategies.test.js
-Game.start({botName: 'KThorBot',
+Game.start({botName: 'KThorBot-2',
     preProcessing: map => {
         Log.log('no data pre-processing performed. number of ships: ' + map.myShips.length)
     },
